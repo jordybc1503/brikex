73d4dbfebd999ab04997b096fb18e2768c515d96
cesar jordi <jordybc15@gmail.com>
2025-01-12 19:53:28 -0500

    tailwind installed

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@popperjs/core", to: "popper.js", preload: true
+pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js", preload: true
