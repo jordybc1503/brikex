2ef6c03fb59196e572f49580e8be20e64cd0aaff
cesar jordi <jordybc15@gmail.com>
2025-01-18 18:11:24 -0500

    home improved

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,10 @@ class Project < ApplicationRecord
   belongs_to :admin
   belongs_to :promotor
   belongs_to :project_type
+
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [50, 50]
+  end
+
+
 end
